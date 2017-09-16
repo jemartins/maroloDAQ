3418bb81ef64213ca0f6284f00fa6d51fa92e7c1
José Eduardo Martins <jemartins@fis.unb.br>
2017-09-16 12:08:17 -0300

    working in PortasGroup actions

---
+++ comserial.cpp.backup
@@ -0,0 +1,195 @@
+#include "comserial.h"
+
+
+/**
+ * @brief CommSerial::CommSerial
+ *
+ * Função do metodo construtor, inicia variaveis e objetos privados para uso da classe
+ *
+ * @param
+ * @return
+ */
+comserial::comserial(QSerialPort *myDev)
+{
+    devSerial = myDev;
+}
+
+
+/**
+ * @brief CommSerial::CarregarDispositivos
+ *
+ * Função que verifica todas as portas disponiveis /dev/tty*
+ * e na sequencia seta a porta com serial.setport e testar se é uma
+ * porta serial
+ *
+ * @param void
+ * @return QStringList com os devices do /dev que são portas seriais
+ */
+QStringList comserial::CarregarDispositivos()
+{
+    QStringList devs;
+
+    foreach (const QSerialPortInfo info, QSerialPortInfo::availablePorts()) {
+
+        devSerial->setPort(info);
+
+        if (devSerial->open(QIODevice::ReadWrite)) {
+            devSerial->close();
+            devs << info.portName();
+        }
+
+    }
+    return devs;
+}
+
+
+
+/**
+ * @brief CommSerial::Conectar
+ *
+ * Função que recebe os parametros Porta, BaudRate e FlowControl e realiza conexão
+ *
+ * @param QString Port(Porta Serial), uint32_t bd(BaudRate), uint8_t fc(FlowControl)
+ * @return
+ */
+bool comserial::Conectar(QString Port, u_int32_t bd)
+{
+    /* Device Serial Port */
+    devSerial->setPortName(Port);
+    //qDebug() << "Dispositivo Porta Serial: " << Port;
+
+
+     /* Conectar SerialPort */
+
+        /* BaudRate */
+        switch (bd) {
+        case 2400:
+            //qDebug() << "Baudrate: 2400";
+            devSerial->setBaudRate(QSerialPort::Baud2400);
+            break;
+        case 4800:
+            //qDebug() << "Baudrate: 4800";
+            devSerial->setBaudRate(QSerialPort::Baud4800);
+            break;
+        case 9600:
+            //qDebug() << "Baudrate: 9600";
+            devSerial->setBaudRate(QSerialPort::Baud9600);
+            break;
+        case 19200:
+            //qDebug() << "Baudrate: 19200";
+            devSerial->setBaudRate(QSerialPort::Baud19200);
+            break;
+        case 115200:
+            //qDebug() << "Baudrate: 115200";
+            devSerial->setBaudRate(QSerialPort::Baud115200);
+            break;
+        }
+
+        /* FlowControl */
+        devSerial->setFlowControl(QSerialPort::NoFlowControl);
+
+
+        /* Configurações adicionais */
+        devSerial->setDataBits(QSerialPort::Data8);
+        devSerial->setParity(QSerialPort::NoParity);
+        devSerial->setStopBits(QSerialPort::OneStop);
+
+
+        if(devSerial->open(QIODevice::ReadWrite)) {
+            //qDebug() << "Porta serial aberta com sucesso!";
+            return true;
+        }
+        else {
+            //qDebug() << "Falha ao abrir porta serial!";
+            //qDebug() << "Erro: " << devSerial->error();
+            return false;
+        }
+
+
+}
+
+
+/**
+ * @brief CommSerial::Desconectar
+ *
+ * Função Desconectar, realiza a limpeza do componente e fecha
+ *
+ * @param
+ * @return
+ */
+bool comserial::Desconectar()
+{
+    devSerial->clear();
+    devSerial->close();
+
+
+    if(devSerial->error() == 0 || !devSerial->isOpen()) {
+        //qDebug() << "Porta serial fechada com sucesso!";
+        return true;
+    }
+    else {
+        //qDebug() << "Falha ao fechar a porta serial! ERRO: " << devSerial->error();
+        return false;
+    }
+
+}
+
+
+/**
+ * @brief CommSerial::Write
+ *
+ * Função para escrever na serial, recebe um ponteiro de caracteres já no formato do const char* do write
+ *
+ * @param const char *cmd
+ * @return void
+ */
+qint64 comserial::Write(const char *cmd)
+{
+    qint64 tamanhoEscrito;
+    tamanhoEscrito = devSerial->write(cmd,qstrlen(cmd));
+
+    return tamanhoEscrito;
+}
+
+
+/**
+ * @brief CommSerial::Read
+ *
+ * Função Realiza a leitura do que chegar pela serial apos escrever na mesma, e devolve um QString
+ *
+ * @param
+ * @return QString
+ */
+QString comserial::Read()
+{
+     QString bufRxSerial;
+     /* Awaits read all the data before continuing */
+
+     while (devSerial->waitForReadyRead(20)) {
+         bufRxSerial += devSerial->readAll();
+     }
+
+     return bufRxSerial;
+}
+
+
+/**
+ * @brief CommSerial::Read
+ *
+ * Função Realiza a leitura do que chegar pela serial apos escrever na mesma, e devolve uma QString
+ * neste caso é enviado um inteiro com o numero de caracteres do buffer de recepção
+ *
+ * @param int
+ * @return QString
+ */
+QString comserial::Read(int TamanhoBuffer)
+{
+    char buf[TamanhoBuffer];
+
+    if (devSerial->canReadLine()) {
+        devSerial->read(buf, sizeof(buf));
+    }
+
+    return buf;
+
+}
---
+++ moc_maroloDAQ.cpp
@@ -0,0 +1,231 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'maroloDAQ.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.6.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "maroloDAQ.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'maroloDAQ.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.6.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_maroloDAQ_t {
+    QByteArrayData data[28];
+    char stringdata0[525];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_maroloDAQ_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_maroloDAQ_t qt_meta_stringdata_maroloDAQ = {
+    {
+QT_MOC_LITERAL(0, 0, 9), // "maroloDAQ"
+QT_MOC_LITERAL(1, 10, 9), // "WriteData"
+QT_MOC_LITERAL(2, 20, 0), // ""
+QT_MOC_LITERAL(3, 21, 4), // "data"
+QT_MOC_LITERAL(4, 26, 13), // "findMaroloDAQ"
+QT_MOC_LITERAL(5, 40, 10), // "scanPortas"
+QT_MOC_LITERAL(6, 51, 14), // "maroloDevClose"
+QT_MOC_LITERAL(7, 66, 8), // "ReadData"
+QT_MOC_LITERAL(8, 75, 21), // "on_btnDevOpen_clicked"
+QT_MOC_LITERAL(9, 97, 22), // "on_btnDevClose_clicked"
+QT_MOC_LITERAL(10, 120, 28), // "on_btnCalibrarSensor_clicked"
+QT_MOC_LITERAL(11, 149, 24), // "on_btnBWTerminal_clicked"
+QT_MOC_LITERAL(12, 174, 21), // "on_btnIniciar_clicked"
+QT_MOC_LITERAL(13, 196, 19), // "on_btnParar_clicked"
+QT_MOC_LITERAL(14, 216, 22), // "on_btnAppClose_clicked"
+QT_MOC_LITERAL(15, 239, 30), // "on_actionSalvar_como_triggered"
+QT_MOC_LITERAL(16, 270, 25), // "on_actionSalvar_triggered"
+QT_MOC_LITERAL(17, 296, 23), // "on_actionSair_triggered"
+QT_MOC_LITERAL(18, 320, 27), // "on_actionConectar_triggered"
+QT_MOC_LITERAL(19, 348, 30), // "on_actionDesconectar_triggered"
+QT_MOC_LITERAL(20, 379, 30), // "on_actionFerramentas_triggered"
+QT_MOC_LITERAL(21, 410, 29), // "on_actionRecarregar_triggered"
+QT_MOC_LITERAL(22, 440, 16), // "setPortasSeriais"
+QT_MOC_LITERAL(23, 457, 8), // "myAction"
+QT_MOC_LITERAL(24, 466, 15), // "toggleStatusbar"
+QT_MOC_LITERAL(25, 482, 8), // "QAction*"
+QT_MOC_LITERAL(26, 491, 6), // "viewst"
+QT_MOC_LITERAL(27, 498, 26) // "on_actionDevices_triggered"
+
+    },
+    "maroloDAQ\0WriteData\0\0data\0findMaroloDAQ\0"
+    "scanPortas\0maroloDevClose\0ReadData\0"
+    "on_btnDevOpen_clicked\0on_btnDevClose_clicked\0"
+    "on_btnCalibrarSensor_clicked\0"
+    "on_btnBWTerminal_clicked\0on_btnIniciar_clicked\0"
+    "on_btnParar_clicked\0on_btnAppClose_clicked\0"
+    "on_actionSalvar_como_triggered\0"
+    "on_actionSalvar_triggered\0"
+    "on_actionSair_triggered\0"
+    "on_actionConectar_triggered\0"
+    "on_actionDesconectar_triggered\0"
+    "on_actionFerramentas_triggered\0"
+    "on_actionRecarregar_triggered\0"
+    "setPortasSeriais\0myAction\0toggleStatusbar\0"
+    "QAction*\0viewst\0on_actionDevices_triggered"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_maroloDAQ[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      22,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,  124,    2, 0x08 /* Private */,
+       4,    0,  127,    2, 0x08 /* Private */,
+       5,    0,  128,    2, 0x08 /* Private */,
+       6,    0,  129,    2, 0x08 /* Private */,
+       7,    0,  130,    2, 0x08 /* Private */,
+       8,    0,  131,    2, 0x08 /* Private */,
+       9,    0,  132,    2, 0x08 /* Private */,
+      10,    0,  133,    2, 0x08 /* Private */,
+      11,    0,  134,    2, 0x08 /* Private */,
+      12,    0,  135,    2, 0x08 /* Private */,
+      13,    0,  136,    2, 0x08 /* Private */,
+      14,    0,  137,    2, 0x08 /* Private */,
+      15,    0,  138,    2, 0x08 /* Private */,
+      16,    0,  139,    2, 0x08 /* Private */,
+      17,    0,  140,    2, 0x08 /* Private */,
+      18,    0,  141,    2, 0x08 /* Private */,
+      19,    0,  142,    2, 0x08 /* Private */,
+      20,    0,  143,    2, 0x08 /* Private */,
+      21,    0,  144,    2, 0x08 /* Private */,
+      22,    1,  145,    2, 0x08 /* Private */,
+      24,    1,  148,    2, 0x08 /* Private */,
+      27,    1,  151,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::QByteArray,    3,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::QString,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   23,
+    QMetaType::Void, 0x80000000 | 25,   26,
+    QMetaType::Void, 0x80000000 | 25,    2,
+
+       0        // eod
+};
+
+void maroloDAQ::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        maroloDAQ *_t = static_cast<maroloDAQ *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->WriteData((*reinterpret_cast< const QByteArray(*)>(_a[1]))); break;
+        case 1: _t->findMaroloDAQ(); break;
+        case 2: _t->scanPortas(); break;
+        case 3: _t->maroloDevClose(); break;
+        case 4: { QString _r = _t->ReadData();
+            if (_a[0]) *reinterpret_cast< QString*>(_a[0]) = _r; }  break;
+        case 5: _t->on_btnDevOpen_clicked(); break;
+        case 6: _t->on_btnDevClose_clicked(); break;
+        case 7: _t->on_btnCalibrarSensor_clicked(); break;
+        case 8: _t->on_btnBWTerminal_clicked(); break;
+        case 9: _t->on_btnIniciar_clicked(); break;
+        case 10: _t->on_btnParar_clicked(); break;
+        case 11: _t->on_btnAppClose_clicked(); break;
+        case 12: _t->on_actionSalvar_como_triggered(); break;
+        case 13: _t->on_actionSalvar_triggered(); break;
+        case 14: _t->on_actionSair_triggered(); break;
+        case 15: _t->on_actionConectar_triggered(); break;
+        case 16: _t->on_actionDesconectar_triggered(); break;
+        case 17: _t->on_actionFerramentas_triggered(); break;
+        case 18: _t->on_actionRecarregar_triggered(); break;
+        case 19: _t->setPortasSeriais((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 20: _t->toggleStatusbar((*reinterpret_cast< QAction*(*)>(_a[1]))); break;
+        case 21: _t->on_actionDevices_triggered((*reinterpret_cast< QAction*(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 20:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QAction* >(); break;
+            }
+            break;
+        case 21:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QAction* >(); break;
+            }
+            break;
+        }
+    }
+}
+
+const QMetaObject maroloDAQ::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_maroloDAQ.data,
+      qt_meta_data_maroloDAQ,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *maroloDAQ::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *maroloDAQ::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_maroloDAQ.stringdata0))
+        return static_cast<void*>(const_cast< maroloDAQ*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int maroloDAQ::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 22)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 22;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 22)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 22;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
