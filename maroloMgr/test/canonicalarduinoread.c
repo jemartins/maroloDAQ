374564d90573166880eef5fc3eef507eae2e2c3b
jemartins <jedumartins@gmail.com>
2019-03-12 11:22:50 -0300

    changed open serial in canonicalarduinoread

--- maroloMgr/test/canonicalarduinoread.c
+++ maroloMgr/test/canonicalarduinoread.c
@@ -1,15 +1,15 @@
 /* www.chrisheydrick.com
-     
-   June 23 2012
-
-   CanonicalArduinoRead write a byte to an Arduino, and then
-   receives a serially transmitted string in response.
-
-   The call/response Arduino sketch is here:
-   https://gist.github.com/2980344
-
-   Arduino sketch details at www.chrisheydrick.com
-*/
+ *     
+ *   June 23 2012
+ * 
+ *   CanonicalArduinoRead write a byte to an Arduino, and then
+ *   receives a serially transmitted string in response.
+ * 
+ *   The call/response Arduino sketch is here:
+ *   https://gist.github.com/2980344
+ * 
+ *   Arduino sketch details at www.chrisheydrick.com
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -22,63 +22,92 @@
 #include <sys/ioctl.h>
 
 #define DEBUG 1
-  
+
 int main(int argc, char *argv[])
 {
-  int fd, n, i;
-  //char buf[64] = "temp text";
-  struct termios toptions;
-
-  /* open serial port */
-  //fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY);
-  fd = open("/dev/ttyACM0",O_RDWR | O_NOCTTY | O_NDELAY);
-  printf("fd opened as %i\n", fd);
-  
-  /* wait for the Arduino to reboot */
-  usleep(3500000);
-  
-  /* get current serial port settings */
-  tcgetattr(fd, &toptions);
-  /* set 9600 baud both ways */
-  cfsetispeed(&toptions, B9600);
-  cfsetospeed(&toptions, B9600);
-  /* 8 bits, no parity, no stop bits */
-  toptions.c_cflag &= ~PARENB;
-  toptions.c_cflag &= ~CSTOPB;
-  toptions.c_cflag &= ~CSIZE;
-  toptions.c_cflag |= CS8;
-  /* Canonical mode */
-  toptions.c_lflag |= ICANON;
-  /* commit the serial port settings */
-  tcsetattr(fd, TCSANOW, &toptions);
-
-  /* Send byte to trigger Arduino to send string back */
-  char buf_call[] = "14\n";
-  //char *buf_call = "14\n";
-  char buf_read[20];
-  //strncpy(buf_call, "14", sizeof(buf_call));
-  //buf_call[sizeof(buf_call) - 1] = '\n';
-  printf("AQUI buf_call = %s", buf_call);
-  write(fd, buf_call, sizeof(buf_call));
-  
-  printf("AQUI passou write, buf_call = %s", buf_call);
-  /* Receive string from Arduino */
-  n = read(fd, &buf_read, 20);
-  printf("AQUI passou read\n");
-  /* insert terminating zero in the string */
-  //buf_read[n] = 0;
-
-  printf("%i bytes read, buffer contains: %s\n", n, buf_read);
- 
-  if(DEBUG)
+    int fd, n, i;
+    /*------------------------------- Opening the Serial Port -------------------------------*/
+    
+    /* Change /dev/ttyUSB0 to the one corresponding to your system */
+    
+    fd = open("/dev/ttyACM0",O_RDWR | O_NOCTTY);	/* ttyUSB0 is the FT232 based USB2SERIAL Converter   */
+    /* O_RDWR   - Read/Write access to serial port       */
+    /* O_NOCTTY - No terminal will control the process   */
+    /* Open in blocking mode,read will wait              */
+    
+    
+    
+    if(fd == -1)						/* Error Checking */
+        printf("\n  Error! in Opening ttyUSB0  ");
+    else
+        printf("\n  ttyUSB0 Opened Successfully ");
+    
+    
+    /*---------- Setting the Attributes of the serial port using termios structure --------- */
+    
+    struct termios SerialPortSettings;	/* Create the structure                          */
+    
+    tcgetattr(fd, &SerialPortSettings);	/* Get the current attributes of the Serial port */
+    
+    /* Setting the Baud rate */
+    cfsetispeed(&SerialPortSettings,B9600); /* Set Read  Speed as 9600                       */
+    cfsetospeed(&SerialPortSettings,B9600); /* Set Write Speed as 9600                       */
+    
+    /* 8N1 Mode */
+    SerialPortSettings.c_cflag &= ~PARENB;   /* Disables the Parity Enable bit(PARENB),So No Parity   */
+    SerialPortSettings.c_cflag &= ~CSTOPB;   /* CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit */
+    SerialPortSettings.c_cflag &= ~CSIZE;	 /* Clears the mask for setting the data size             */
+    SerialPortSettings.c_cflag |=  CS8;      /* Set the data bits = 8                                 */
+    
+    SerialPortSettings.c_cflag &= ~CRTSCTS;       /* No Hardware flow Control                         */
+    SerialPortSettings.c_cflag |= CREAD | CLOCAL; /* Enable receiver,Ignore Modem Control lines       */ 
+    
+    
+    SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          /* Disable XON/XOFF flow control both i/p and o/p */
+    SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);  /* Non Cannonical mode                            */
+    
+    SerialPortSettings.c_oflag &= ~OPOST;/*No Output Processing*/
+    
+    /* Setting Time outs */
+    SerialPortSettings.c_cc[VMIN] = 10; /* Read at least 10 characters */
+    SerialPortSettings.c_cc[VTIME] = 0; /* Wait indefinetly   */
+    
+    
+    if((tcsetattr(fd,TCSANOW,&SerialPortSettings)) != 0) /* Set the attributes to the termios structure*/
+        printf("\n  ERROR ! in Setting attributes");
+    else
+        printf("\n  BaudRate = 9600 \n  StopBits = 1 \n  Parity   = none");
+    
+    /*------------------------------- Read data from serial port -----------------------------*/
+    
+    tcflush(fd, TCIFLUSH);   /* Discards old data in the rx buffer            */
+    /* Send byte to trigger Arduino to send string back */
+    char buf_call[] = "14\n";
+    char buf_read[20];
+    //strncpy(buf_call, "14", sizeof(buf_call));
+    //buf_call[sizeof(buf_call)] = '\n';
+    printf("AQUI buf_call = %s", buf_call);
+    //tcflush(fd, TCIFLUSH);
+    write(fd, buf_call, sizeof(buf_call));
+    
+    printf("AQUI passou write, buf_call = %s", buf_call);
+    /* Receive string from Arduino */
+    n = read(fd, &buf_read, 20);
+    printf("AQUI passou read\n");
+    /* insert terminating zero in the string */
+    //buf_read[n] = 0;
+    
+    printf("%i bytes read, buffer contains: %s\n", n, buf_read);
+    
+    if(DEBUG)
     {
-      printf("Printing individual characters in buf as integers...\n\n");
-      for(i=0; i<n; i++)
-	{
-	  printf("Byte %i:%i, ",i+1, (int)buf_read[i]);
-	}
-      printf("\n");
+        printf("Printing individual characters in buf as integers...\n\n");
+        for(i=0; i<n; i++)
+        {
+            printf("Byte %i:%i, ",i+1, (int)buf_read[i]);
+        }
+        printf("\n");
     }
-
-  return 0;
+    
+    return 0;
 }
